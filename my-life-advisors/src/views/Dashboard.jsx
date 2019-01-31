e128a7fbf61d7210834122b5616e762f29c7692b
Vu Le <vuleuml2011@gmail.com>
2019-01-30 18:36:41 -0600

    adding in template

---
+++ my-life-advisors/src/views/Dashboard.jsx
@@ -0,0 +1,260 @@
+import React from "react";
+// nodejs library that concatenates classes
+import classNames from "classnames";
+
+
+// reactstrap components
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardTitle,
+  Table,
+  Row,
+  Col
+} from "reactstrap";
+
+
+class Dashboard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bigChartData: "data1"
+    };
+  }
+  setBgChartData = name => {
+    this.setState({
+      bigChartData: name
+    });
+  };
+  render() {
+    return (
+      <>
+        <div className="content">
+          <Row>
+            <Col xs="12">
+              <Card className="card-chart">
+                <CardHeader>
+                  <Row>
+                    <Col className="text-left" sm="6">
+                      <h5 className="card-category">Total Shipments</h5>
+                      <CardTitle tag="h2">Performance</CardTitle>
+                    </Col>
+                    <Col sm="6">
+                      {/* <ButtonGroup
+                        className="btn-group-toggle float-right"
+                        data-toggle="buttons"
+                      >
+                        <Button
+                          tag="label"
+                          className={classNames("btn-simple", {
+                            active: this.state.bigChartData === "data1"
+                          })}
+                          color="info"
+                          id="0"
+                          size="sm"
+                          onClick={() => this.setBgChartData("data1")}
+                        >
+                          <input
+                            defaultChecked
+                            className="d-none"
+                            name="options"
+                            type="radio"
+                          />
+                          <span className="d-none d-sm-block d-md-block d-lg-block d-xl-block">
+                            Accounts
+                          </span>
+                          <span className="d-block d-sm-none">
+                            <i className="tim-icons icon-single-02" />
+                          </span>
+                        </Button>
+                        <Button
+                          color="info"
+                          id="1"
+                          size="sm"
+                          tag="label"
+                          className={classNames("btn-simple", {
+                            active: this.state.bigChartData === "data2"
+                          })}
+                          onClick={() => this.setBgChartData("data2")}
+                        >
+                          <input
+                            className="d-none"
+                            name="options"
+                            type="radio"
+                          />
+                          <span className="d-none d-sm-block d-md-block d-lg-block d-xl-block">
+                            Purchases
+                          </span>
+                          <span className="d-block d-sm-none">
+                            <i className="tim-icons icon-gift-2" />
+                          </span>
+                        </Button>
+                        <Button
+                          color="info"
+                          id="2"
+                          size="sm"
+                          tag="label"
+                          className={classNames("btn-simple", {
+                            active: this.state.bigChartData === "data3"
+                          })}
+                          onClick={() => this.setBgChartData("data3")}
+                        >
+                          <input
+                            className="d-none"
+                            name="options"
+                            type="radio"
+                          />
+                          <span className="d-none d-sm-block d-md-block d-lg-block d-xl-block">
+                            Sessions
+                          </span>
+                          <span className="d-block d-sm-none">
+                            <i className="tim-icons icon-tap-02" />
+                          </span>
+                        </Button>
+                      </ButtonGroup> */}
+                    </Col>
+                  </Row>
+                </CardHeader>
+                <CardBody>
+                  {/* <div className="chart-area">
+                    <Line
+                      data={chartExample1[this.state.bigChartData]}
+                      options={chartExample1.options}
+                    />
+                  </div> */}
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+          <Row>
+            <Col lg="4">
+              <Card className="card-chart">
+                <CardHeader>
+                  <h5 className="card-category">Total Shipments</h5>
+                  <CardTitle tag="h3">
+                    <i className="tim-icons icon-bell-55 text-info" />{" "}
+                    763,215
+                  </CardTitle>
+                </CardHeader>
+                <CardBody>
+                  {/* <div className="chart-area">
+                    <Line
+                      data={chartExample2.data}
+                      options={chartExample2.options}
+                    />
+                  </div> */}
+                </CardBody>
+              </Card>
+            </Col>
+            <Col lg="4">
+              <Card className="card-chart">
+                <CardHeader>
+                  <h5 className="card-category">Daily Sales</h5>
+                  <CardTitle tag="h3">
+                    <i className="tim-icons icon-delivery-fast text-primary" />{" "}
+                    3,500€
+                  </CardTitle>
+                </CardHeader>
+                <CardBody>
+                  {/* <div className="chart-area">
+                    <Bar
+                      data={chartExample3.data}
+                      options={chartExample3.options}
+                    />
+                  </div> */}
+                </CardBody>
+              </Card>
+            </Col>
+            <Col lg="4">
+              <Card className="card-chart">
+                <CardHeader>
+                  <h5 className="card-category">Completed Tasks</h5>
+                  <CardTitle tag="h3">
+                    <i className="tim-icons icon-send text-success" /> 12,100K
+                  </CardTitle>
+                </CardHeader>
+                <CardBody>
+                  {/* <div className="chart-area">
+                    <Line
+                      data={chartExample4.data}
+                      options={chartExample4.options}
+                    />
+                  </div> */}
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+          <Row>
+            
+            <Col lg="6" md="12">
+              <Card>
+                <CardHeader>
+                  <CardTitle tag="h4">Simple Table</CardTitle>
+                </CardHeader>
+                <CardBody>
+                  <Table className="tablesorter" responsive>
+                    <thead className="text-primary">
+                      <tr>
+                        <th>Name</th>
+                        <th>Country</th>
+                        <th>City</th>
+                        <th className="text-center">Salary</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>Dakota Rice</td>
+                        <td>Niger</td>
+                        <td>Oud-Turnhout</td>
+                        <td className="text-center">$36,738</td>
+                      </tr>
+                      <tr>
+                        <td>Minerva Hooper</td>
+                        <td>Curaçao</td>
+                        <td>Sinaai-Waas</td>
+                        <td className="text-center">$23,789</td>
+                      </tr>
+                      <tr>
+                        <td>Sage Rodriguez</td>
+                        <td>Netherlands</td>
+                        <td>Baileux</td>
+                        <td className="text-center">$56,142</td>
+                      </tr>
+                      <tr>
+                        <td>Philip Chaney</td>
+                        <td>Korea, South</td>
+                        <td>Overland Park</td>
+                        <td className="text-center">$38,735</td>
+                      </tr>
+                      <tr>
+                        <td>Doris Greene</td>
+                        <td>Malawi</td>
+                        <td>Feldkirchen in Kärnten</td>
+                        <td className="text-center">$63,542</td>
+                      </tr>
+                      <tr>
+                        <td>Mason Porter</td>
+                        <td>Chile</td>
+                        <td>Gloucester</td>
+                        <td className="text-center">$78,615</td>
+                      </tr>
+                      <tr>
+                        <td>Jon Porter</td>
+                        <td>Portugal</td>
+                        <td>Gloucester</td>
+                        <td className="text-center">$98,615</td>
+                      </tr>
+                    </tbody>
+                  </Table>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+        </div>
+      </>
+    );
+  }
+}
+
+export default Dashboard;
