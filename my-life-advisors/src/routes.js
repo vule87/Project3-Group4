1244521cf4a83b233683b5fcae840ae341a5fb85
Alijan Shiraliyev <aj@Alijans-MacBook-Pro.local>
2019-02-02 10:42:57 -0600

    working template

---
+++ my-life-advisors/src/routes.js
@@ -0,0 +1,31 @@
+import Dashboard from "views/Dashboard.jsx";
+import TableList from "views/TableList.jsx";
+import UserProfile from "views/UserProfile.jsx";
+
+var routes = [
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+    rtlName: "لوحة القيادة",
+    icon: "tim-icons icon-chart-pie-36",
+    component: Dashboard,
+    layout: "/admin"
+  },
+  {
+    path: "/user-profile",
+    name: "User Profile",
+    rtlName: "ملف تعريفي للمستخدم",
+    icon: "tim-icons icon-single-02",
+    component: UserProfile,
+    layout: "/admin"
+  },
+  {
+    path: "/tables",
+    name: "Favorited List",
+    rtlName: "قائمة الجدول",
+    icon: "tim-icons icon-puzzle-10",
+    component: TableList,
+    layout: "/admin"
+  }
+];
+export default routes;
