f3542fd4d143bdb04cb6eb1df9fba0040adbc441
Alijan Shiraliyev <aj@Alijans-MacBook-Pro.local>
2019-02-04 13:17:17 -0600

    changed rendering ajax results

--- models/favorite.js
+++ models/favorite.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const favoriteSchema = new Schema({
-    title: { type: String},
+    character: { type: String},
     quote: { type: String},
 });
 
