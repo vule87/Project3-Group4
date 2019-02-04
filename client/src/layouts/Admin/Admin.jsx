65a6611ff1275bebbbdf4052d26ef87b2ab12f81
Alijan Shiraliyev <aj@Alijans-MacBook-Pro.local>
2019-02-04 12:03:51 -0600

    disabled fixedplugin

--- client/src/layouts/Admin/Admin.jsx
+++ client/src/layouts/Admin/Admin.jsx
@@ -7,7 +7,7 @@ import PerfectScrollbar from "perfect-scrollbar";
 import AdminNavbar from "components/Navbars/AdminNavbar.jsx";
 import Footer from "components/Footer/Footer.jsx";
 import Sidebar from "components/Sidebar/Sidebar.jsx";
-import FixedPlugin from "components/FixedPlugin/FixedPlugin.jsx";
+// import FixedPlugin from "components/FixedPlugin/FixedPlugin.jsx";
 
 import routes from "routes.js";
 
@@ -116,10 +116,10 @@ class Admin extends React.Component {
             )}
           </div>
         </div>
-        <FixedPlugin
+        {/* <FixedPlugin
           bgColor={this.state.backgroundColor}
           handleBgClick={this.handleBgClick}
-        />
+        /> */}
       </>
     );
   }
